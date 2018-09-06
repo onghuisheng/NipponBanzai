80216408997aeaeb1c52bfed6d3b2c9fe5265d02
HoopAWolf <hoopawolf394@gmail.com>
2018-09-06 17:14:01 +0800

    fix
    fix

--- Assets/Scripts/Util/ObjectPool.cs
+++ Assets/Scripts/Util/ObjectPool.cs
@@ -113,12 +113,14 @@ public class ObjectPool : Singleton<ObjectPool>
     public GameObject GetEntityPlayer()
     {
         if (!go_player_instance.activeSelf)
+        {
             go_player_instance.SetActive(true);
 
-        go_player_instance.GetComponent<EntityPlayer>().SetPosition(new Vector3(0, 1, 0));
+            go_player_instance.GetComponent<EntityPlayer>().SetPosition(new Vector3(0, 1, 0));
 
-        go_player_instance.GetComponent<CharacterMovement>().go_camera = GameObject.Find("Main Camera");
+            go_player_instance.GetComponent<CharacterMovement>().go_camera = GameObject.Find("Main Camera");
 
+        }
         return go_player_instance;
     }
 
