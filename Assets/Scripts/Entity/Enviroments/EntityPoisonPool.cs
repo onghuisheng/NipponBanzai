ab54bc485bea59b54983dfef81d85d7122d9137b
Hui Sheng @ FYP Lab <deathezer10@gmail.com>
2018-09-12 17:14:51 +0800

    Initial poison pool

--- Assets/Scripts/Entity/Enviroments/EntityPoisonPool.cs
+++ Assets/Scripts/Entity/Enviroments/EntityPoisonPool.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityPoisonPool : EntityEnviroment {
+public class EntityPoisonPool : EntityEnviroment
+{
 
-    public override void SetUpObjectWLifeTime(float _lifetime, Vector3 _pos, Vector3 _size, bool _isStatic = true)
+    public void SetUpPoisonPoolWLifeTime(float _damageTick, float _lifetime, Vector3 _pos, Vector3 _size, bool _isStatic = true)
     {
         base.SetUpObjectWLifeTime(_lifetime, _pos, _size, _isStatic);
 
-        // SetUpHitBox(gameObject.name, gameObject.tag, gameObject.GetInstanceID().ToString(), );
+        SetUpHitBox(gameObject.name, gameObject.tag, gameObject.GetInstanceID().ToString(), _damageTick, _size, _pos, Quaternion.Euler(0, Random.Range(0, 360), 0), _lifetime, 0.1f);
     }
-    
+
 }
--- Assets/Scripts/Entity/Projectiles/ArcBulllet.cs
+++ Assets/Scripts/Entity/Projectiles/ArcBulllet.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArcBulllet : EntityProjectiles {
+public class ArcBulllet : EntityProjectiles
+{
     private Vector3
         v3_startPos,
         v3_endPos;
@@ -12,8 +13,10 @@ public class ArcBulllet : EntityProjectiles {
         f_height,
         f_incrementor;
 
+    System.Action<Collider>
+        act_onTriggerEnter;
 
-    public void SetUpProjectile(float _lifetime, float _damage, float _speed, float _height, Vector3 _start, Vector3 _end, Vector3 _size, GameObject _source)
+    public void SetUpProjectile(float _lifetime, float _damage, float _speed, float _height, Vector3 _start, Vector3 _end, Vector3 _size, GameObject _source, System.Action<Collider> _onTriggerEnter = null)
     {
         base.SetUpProjectile(_source, _lifetime, _damage, _speed, _size, new Vector3(0, 0, 0));
 
@@ -23,15 +26,18 @@ public class ArcBulllet : EntityProjectiles {
         v3_endPos = _end;
         f_lifeElapse = 0;
         f_incrementor = 0;
+        act_onTriggerEnter = _onTriggerEnter;
     }
 
     // Use this for initialization
-    protected override void Start () {
-		
-	}
+    protected override void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    protected override void Update () {
+    protected override void Update()
+    {
         //f_lifeElapse += Time.deltaTime;
 
         //if (f_lifeElapse > F_lifetime)
@@ -74,4 +80,11 @@ public class ArcBulllet : EntityProjectiles {
             gameObject.SetActive(false);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (act_onTriggerEnter != null)
+            act_onTriggerEnter.Invoke(other);
+    }
+
 }
