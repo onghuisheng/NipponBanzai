428a2fe7ee357adc8b31ed4416d39b7427121583
Hui Sheng @ FYP Lab <deathezer10@gmail.com>
2018-09-17 14:51:17 +0800

    Fixed miniboss spawning shit ton of enemies Initial particlehandler

--- Assets/Scripts/Entity/Enviroments/EntityPoisonPool.cs
+++ Assets/Scripts/Entity/Enviroments/EntityPoisonPool.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class EntityPoisonPool : EntityEnviroment
 {
 
+    protected override void Start()
+    {
+        base.Start();
+    }
+
     public void SetUpPoisonPoolWLifeTime(GameObject _owner, float _damageTick, float _lifetime, Vector3 _pos, bool _isStatic = true)
     {
         base.SetUpObjectWLifeTime(_lifetime, _pos, Vector3.one, _isStatic);
 
-        ParticleSystem ps = transform.GetChild(0).GetComponent<ParticleSystem>();
-        ps.Clear();
-        ps.Simulate(5);
-        ps.Play();
+        ParticleSystem ps = ParticleHandler.GetInstance().SpawnParticle(ParticleHandler.ParticleType.PoisonCloud, null, _pos, Quaternion.identity.eulerAngles, _lifetime).GetComponent<ParticleSystem>();
 
         Vector3 boundSize = ps.GetComponent<Renderer>().bounds.size * 0.25f;
         SetUpHitBox(_owner.name, _owner.tag, _owner.GetInstanceID().ToString(), _damageTick, boundSize, _pos, Quaternion.Euler(0, Random.Range(0, 360), 0), _lifetime, 0.1f);
