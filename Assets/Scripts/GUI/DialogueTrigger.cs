259a600fd84f1da0466aa9051c429ea9e0a4fe70
Hui Sheng @ FYP Lab <deathezer10@gmail.com>
2018-09-17 18:06:42 +0800

    Initial dialogue system by ester

---
+++ Assets/Scripts/GUI/DialogueTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DemDialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public bool DestroyOnTrigger = true;
+
+    private DialogueHandler dialogueHandler;
+
+    void Start()
+    {
+        dialogueHandler = GameObject.FindWithTag("DialogueHandler").GetComponent<DialogueHandler>();
+    }
+
+    public void TriggerDialogue()
+    {
+        dialogueHandler.StartDialogue(dialogue);
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TriggerDialogue();
+
+            if (DestroyOnTrigger)
+                Destroy(gameObject);
+        }
+    }
+
+}
