be9f50770a67a9ca0daa2915338f4aed74f89650
Hui Sheng @ Home <deathezer10@gmail.com>
2018-09-26 17:25:01 +0800

    ++Save data for main menu ++Level 2 terrain texture

--- Assets/Scripts/GUI/UIMainMenuAssistant.cs
+++ Assets/Scripts/GUI/UIMainMenuAssistant.cs
@@ -10,6 +10,10 @@ public class UIMainMenuAssistant : MonoBehaviour
 
     public GameObject m_MainMenuElements;
     public Image m_FadeTexture;
+    public Slider m_SFXSlider;
+    public Slider m_MusicSlider;
+    public List<Image> m_GraphicsLevel; // order matters - index 0 should be low graphics
+
 
     private RectTransform m_ElementsTransform;
     private Canvas m_ElementCanvas;
@@ -21,6 +25,11 @@ public class UIMainMenuAssistant : MonoBehaviour
 
         if (m_ElementCanvas == null)
             Debug.LogError("Can't find canvas, this script will not work!");
+
+        SaveDataSettings settings = SaveData.LoadSettings();
+        m_SFXSlider.value = settings.m_SFXVolume;
+        m_MusicSlider.value = settings.m_MusicVolume;
+        m_GraphicsLevel[settings.m_GraphicsLevel].gameObject.SetActive(true);
     }
 
     public void MainMenuToOptions()
@@ -31,6 +40,22 @@ public class UIMainMenuAssistant : MonoBehaviour
     public void OptionsToMainMenu()
     {
         m_ElementsTransform.DOAnchorPosY(0, 1).SetEase(Ease.InOutBack);
+    }
+
+    public void SaveSettings()
+    {
+        int graphicsLevel = 0;
+
+        for (int i = 0; i < m_GraphicsLevel.Count; ++i)
+        {
+            if (m_GraphicsLevel[i].gameObject.activeInHierarchy)
+            {
+                graphicsLevel = i;
+                break;
+            }
+        }
+
+        SaveData.SaveSettings(m_SFXSlider.value, m_MusicSlider.value, graphicsLevel);
     }
 
     public void StartGame()
--- Assets/Scripts/Util/SaveData.cs
+++ Assets/Scripts/Util/SaveData.cs
@@ -1,23 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SaveData
 {
 
-    public enum DataString
+    private const string m_SettingsFileName = "UserSettings.json";
+    private const string m_InventoryFileName = "UserInventory.json";
+
+    private static string GetSaveDirectory() { return Application.persistentDataPath; }
+
+    private static bool SaveDataExists(string fileName)
     {
-        SFXVolume,
-        MusicVolume,
-        GraphicsLevel,
+        string fullFilePath = Path.Combine(GetSaveDirectory(), fileName);
+        return File.Exists(fullFilePath);
     }
 
     public static void SaveSettings(float sfxVolume, float musicVolume, int graphicsLevel)
     {
-        PlayerPrefs.SetFloat(DataString.SFXVolume.ToString(), sfxVolume);
-        PlayerPrefs.SetFloat(DataString.MusicVolume.ToString(), musicVolume);
-        PlayerPrefs.SetInt(DataString.GraphicsLevel.ToString(), graphicsLevel);
+        string fullFilePath = Path.Combine(GetSaveDirectory(), m_SettingsFileName);
+        SaveDataSettings settings = new SaveDataSettings()
+        {
+            m_GraphicsLevel = graphicsLevel,
+            m_SFXVolume = sfxVolume,
+            m_MusicVolume = musicVolume
+        };
 
+        File.WriteAllText(fullFilePath, JsonUtility.ToJson(settings));
+        Debug.LogFormat("Saved {0} to {1}", m_SettingsFileName, fullFilePath);
     }
 
+    public static SaveDataSettings LoadSettings()
+    {
+        if (SaveDataExists(m_SettingsFileName))
+        {
+            string fullFilePath = Path.Combine(GetSaveDirectory(), m_SettingsFileName);
+            string jsonData = File.ReadAllText(fullFilePath);
+            Debug.LogFormat("Loaded {0} from {1}", m_SettingsFileName, fullFilePath);
+            return JsonUtility.FromJson<SaveDataSettings>(jsonData);
+        }
+        else
+        {
+            return new SaveDataSettings();
+        }
+    }
+
+}
+
+[Serializable]
+public class SaveDataSettings
+{
+    public float m_SFXVolume = 1;
+    public float m_MusicVolume = 1;
+    public int m_GraphicsLevel = 2;
+}
+
+[Serializable]
+public class SaveDataInventory
+{
+    public Dictionary<Item.ITEM_TYPE, int> m_Inventory;
 }
